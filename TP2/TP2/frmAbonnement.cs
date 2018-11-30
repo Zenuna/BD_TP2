067b9417a32624c59857f5f9b524bad9ab01d46f
Franc <Franc@LAPTOP-FP>
2018-11-30 12:00:41 -0500

    Niko Fuk up

--- TP2/TP2/frmAbonnement.cs
+++ TP2/TP2/frmAbonnement.cs
@@ -11,10 +11,34 @@ using System.Windows.Forms;
 namespace TP2
 {
     public partial class frmAbonnement : Form
+
     {
+        DataClasses1DataContext dataClasses1DataContext = new DataClasses1DataContext();
+        Employe empConnecter;
         public frmAbonnement(Employe empConnect)
         {
             InitializeComponent();
+            empConnecter = empConnect;
+        }
+
+        private void frmAbonnement_Load(object sender, EventArgs e)
+        {
+            typesAbonnementBindingSource.DataSource = dataClasses1DataContext.TypesAbonnements;
+            dateNaissanceDateTimePicker.MaxDate = DateTime.Now.Date.AddYears(-18);
+            lblDateAbonnement.Text = DateTime.Now.Date.ToShortDateString();           
+        }
+
+        private void cmbTypeAbonnement_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Int32.Parse(cmbTypeAbonnement.SelectedValue.ToString()) == 2)
+            {
+                dateNaissanceDateTimePicker.MaxDate = DateTime.Now.Date.AddYears(-60);
+                MessageBox.Show("Test", "Test");
+            }
+            else
+            {
+                dateNaissanceDateTimePicker.MaxDate = DateTime.Now.Date.AddYears(-18);
+            }
         }
     }
 }
