0210cca8865121e39c054c30468c57c48ee7cbbc
Franc <Franc@LAPTOP-FP>
2018-12-02 20:19:31 -0500

    fini parties jouées

--- TP2/TP2/frmInscriptionPartieGolf.cs
+++ TP2/TP2/frmInscriptionPartieGolf.cs
@@ -12,9 +12,62 @@ namespace TP2
 {
     public partial class frmInscriptionPartieGolf : Form
     {
+
+        DataClasses1DataContext dataContext = new DataClasses1DataContext();
         public frmInscriptionPartieGolf(Employe empConnect)
         {
             InitializeComponent();
+        }
+
+        private void frmInscriptionPartieGolf_Load(object sender, EventArgs e)
+        {
+            terrainBindingSource.DataSource = dataContext.Terrains;
+            lblDatePartie.Text = DateTime.Now.Date.ToShortDateString();
+            nomCompletIDAbonneBindingSource.DataSource = infoIDetNom();
+        }
+
+        private List<nomCompletIDAbonne> infoIDetNom()
+        {
+            DateTime dateTime = DateTime.Now;          
+
+            var test = from Abonnements in dataContext.Abonnements
+                       join Reabonnements in dataContext.Reabonnements on Abonnements.Id equals Reabonnements.IdAbonnement into lesAbonnesValides
+                       from Reabonnements in lesAbonnesValides.DefaultIfEmpty()
+                       where
+                         Abonnements.DateAbonnement > DateTime.Now.Date.AddYears(-1) ||
+                        Reabonnements.DateRenouvellement > DateTime.Now.Date.AddYears(-1)
+                       select Abonnements;
+            
+            List <nomCompletIDAbonne> lstNomIDValides = new List<nomCompletIDAbonne>();
+            foreach (var prixDepenses in test)
+            {                
+                lstNomIDValides.Add(new nomCompletIDAbonne(prixDepenses.Id,prixDepenses.Id.PadRight(8, ' ') + " => "+prixDepenses.Prenom +" "+ prixDepenses.Nom));
+            }
+
+            return lstNomIDValides;
+        }
+
+        private void btnEnregistrement_Click(object sender, EventArgs e)
+        {
+            var nouvelPartie = new PartiesJouee
+            {
+                IdAbonnement = cmbAbonne.SelectedValue.ToString(),
+                NoTerrain = Convert.ToInt32(cmbTerrain.SelectedValue),
+                DatePartie = Convert.ToDateTime(lblDatePartie.Text),
+                Pointage = Convert.ToInt32(nUDPointage.Value),
+                Remarque = remarqueTextBox.Text
+            };
+
+            dataContext.PartiesJouees.InsertOnSubmit(nouvelPartie);
+            try
+            {
+                dataContext.SubmitChanges();
+                MessageBox.Show($"La partie jouée le {lblDatePartie.Text} par {cmbAbonne.SelectedValue.ToString()} a été enregistrée", "Enregistrement d'une partie jouée", MessageBoxButtons.OK, MessageBoxIcon.Information);              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("L'enregistrement de la partie a échouée" + ex, "Enregistrement de l'abonné principal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
---
+++ TP2/TP2/nomCompletIDAbonne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class nomCompletIDAbonne
+    {
+        public string idAbonnement { get; private set; }
+        public string idEtNomComplet { get; private set; }
+        public nomCompletIDAbonne(string idAbonnement, string idEtNomComplet)
+        {
+            this.idAbonnement = idAbonnement;
+            this.idEtNomComplet = idEtNomComplet;
+        }
+    }
+}
