8b42f1eba23e1d2b7ef52bdcf7a683f8b440bfc9
Nicolas Jouanique <N.Jouanique@cgodin.qc.ca>
2018-12-04 18:31:45 -0500

    Update script

--- SCRIPT_CRÉATION.sql
+++ SCRIPT_CRÉATION.sql
@@ -115,13 +115,7 @@ CREATE TABLE Employes(
 	CONSTRAINT FK_SexeEmploye FOREIGN KEY(Sexe) REFERENCES Sexe(idSexe)
 );
 INSERT INTO Employes (No,MotDePasse,Nom,Prenom,Sexe,Age,NoCivique,Rue,Ville,IdProvince,CodePostal,Telephone,Cellulaire,Courriel,SalaireHoraire,NoTypeEmploye) 
-VALUES (1,'','Admin','Admin','H',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,1),
-(2,'','Admin','Admin','H',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,2),
-(3,'','Admin','Admin','F',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,3),
-(4,'','Admin','Admin','F',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,4),
-(5,'','Admin','Admin','F',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,5),
-(6,'','Admin','Admin','H',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,6),
-(7,'','Admin','Admin','H',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,7);
+VALUES (1,'','Admin','Admin','H',66,666,'Hell St','Helltown','NT','A1B2C3','6666666666',null,'lucifer@cgodin.qc.ca',666,1);
 SELECT * From Employes;
 
 CREATE TABLE Services(
