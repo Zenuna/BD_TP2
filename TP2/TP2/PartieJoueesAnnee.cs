d2c295d0d2ca196f9906675fe194c769ad027572
Franc <Franc@LAPTOP-FP>
2018-12-03 19:14:05 -0500

    Rapport Fini

---
+++ TP2/TP2/PartieJoueesAnnee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class PartieJoueesAnnee
+    {
+        public int Annee { get; set; }
+        public string nomTerrain { get; set; }
+        public DateTime datePartie { get; set; }
+        public string nomAbonnePrincipal { get; set; }
+        public int pointage { get; set; }
+    }
+}
---
+++ TP2/TP2/RapportDepensesEmploye.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class RapportDepensesEmploye
+    {
+        public string nomCompletEmploye { get; set; }
+        public string typeService { get; set; }
+        public decimal montant { get; set; }
+        public DateTime dateDepense { get; set; }
+        public string nomCompletAbPrincipal { get; set; }
+    }
+}
